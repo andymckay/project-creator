90269252dd05987ba9a6fc22651fe563df16c78e
Andy McKay <andymckay@github.com>
2020-02-28 17:28:06 -0800

    first commit

---
+++ project.js
@@ -0,0 +1,76 @@
+let token = process.env.INPUT_TOKEN;
+let fs = require('fs');
+let https = require('https');
+let destination = process.env.INPUT_DESTINATION;
+let structure = JSON.parse(process.env.INPUT_STRUCTURE);
+var querystring = require('querystring');
+
+function call(url, data) {
+    let json = JSON.stringify(data);
+    let options = {
+        'headers': {
+            'Accept': 'application/vnd.github.inertia-preview+json',
+            'Authorization': `token ${token}`,
+            'Content-Type': 'application/json',
+            'User-Agent': 'actions/create-project',
+            'Content-Length': Buffer.byteLength(json)
+        },
+        'host': 'api.github.com',
+        'path': url,
+        'method': 'POST'
+    }
+
+    return new Promise((resolve, reject) => {
+        let post = https.request(options, (res) => {
+            let data = "";
+            if (res.statusCode !== 201) {
+                console.log(`Got an error: ${res.statusCode}`)
+                process.exit(1);
+            }
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                data += chunk;
+            })
+            res.on('end', function () {
+                resolve(JSON.parse(data));
+            });
+            res.on('error', (error) => {
+                reject(error);
+            })
+        });
+
+        post.write(json);
+        post.end();
+    });
+}
+
+
+let projectData = {
+    'name' : structure['project']['name'],
+    'body': structure['project']['body']
+}
+call(`/repos/${destination}/projects`, projectData)
+.then((res) => {
+    console.log(`Created project: ${res.html_url}`)
+    return res;
+}).then((project) => {
+    function processColumns(columns) {
+        var index = 0;
+        function next() {
+            if (index < columns.length) {
+                let column = columns[index++];
+                call(
+                    `/projects/${project.id}/columns`,
+                    {'name' : column['name']}
+                ).then((res) => {
+                    return call(
+                        `/projects/columns/${res.id}/cards`,
+                        {'note': column['card']}
+                    )
+                }).then(next)
+            }
+        }
+        next();
+    }
+    processColumns(structure['columns'])
+})
